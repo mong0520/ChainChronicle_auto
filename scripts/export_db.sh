a3e3a6bb704933fe6c043b94561babce1eecb63f
MongCheng <mongcheng@gmail.com>
2015-11-25 16:58:57 +0800

    add export/import script for mongodb

---
+++ scripts/export_db.sh
@@ -0,0 +1,2 @@
+mongoexport --db cc --collection charainfo --out charainfo.db
+mongoexport --db cc --collection chararein --out chararein.db
---
+++ scripts/import_db.sh
@@ -0,0 +1,2 @@
+mongoimport --collection charaein --db cc --file db/chararein.db
+mongoimport --collection chararein --db cc --file db/chararein.db
