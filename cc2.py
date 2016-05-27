c14077112f753f54e20d1779397ed7f5392bf79f
Neil_Wei <mongcheng@gmail.com>
2016-05-27 14:18:32 +0800

    update

--- cc2.py
+++ cc2.py
@@ -540,14 +540,14 @@ class ChainChronicleAutomation():
                 wave_list.append(data['max_wave'])
             self.logger.debug("關卡id = {0}".format(base_id_list))
             # Start
-            pt_cid = [7024, 46, 7017, 41]
+            pt_cid = [5002, 6043, 7017, 41]
             for idx, bid in enumerate(base_id_list):
                 now = int(time.time()*1000)
                 hexNow = format(now + 5000, 'x')
                 self.logger.debug(u"Using Party {0}".format(idx))
 
                 # Start entry
-                self.logger.debug("")
+                self.logger.debug("開始討伐")
                 self.headers = {
                     'Cookie': 'sid={0}'.format(self.sid),
                     'nat': "bid={3}&brave=0&cnt={0}&fid=383974&full=0&jid={2}&nature=bid%3d{3}%26brave%3d0%26cnt%3d{0}%26fid%3d383974%26full%3d0%26jid%3d{2}%26pt%3d0&pt={4}&timestamp={1}".format(hexNow, now, jid, bid, idx)}
@@ -570,7 +570,7 @@ class ChainChronicleAutomation():
 
                 payload = "mission=%7b%22cid%22%3a%5b{4}%5d%2c%22sid%22%3a%5b0%5d%2c%22fid%22%3a4006%2c%22ms%22%3a0%2c%22md%22%3a18179%2c%22sc%22%3a%7b%221%22%3a1%2c%222%22%3a0%2c%223%22%3a0%2c%224%22%3a0%7d%2c%22es%22%3a0%2c%22at%22%3a1%2c%22he%22%3a0%2c%22da%22%3a1%2c%22ba%22%3a0%2c%22bu%22%3a0%2c%22job%22%3a%7b%220%22%3a1%2c%221%22%3a1%2c%222%22%3a0%2c%223%22%3a0%2c%224%22%3a0%7d%2c%22weapon%22%3a%7b%220%22%3a1%2c%221%22%3a0%2c%222%22%3a0%2c%223%22%3a1%2c%224%22%3a0%2c%225%22%3a0%2c%228%22%3a0%2c%229%22%3a0%2c%2210%22%3a0%7d%2c%22box%22%3a1%2c%22um%22%3a%7b%221%22%3a0%2c%222%22%3a0%2c%223%22%3a2%7d%2c%22fj%22%3a0%2c%22fw%22%3a0%2c%22fo%22%3a0%2c%22cc%22%3a1%2c%22bf_atk%22%3a0%2c%22bf_hp%22%3a0%2c%22bf_spd%22%3a0%7d&nature=bid%3d{3}%26bt%3d6176%26cc%3d1%26cnt%3d{0}%26d%3d1%26jid%3d{2}%26mission%3d%257b%2522cid%2522%253a%255b{4}%255d%252c%2522sid%2522%253a%255b0%255d%252c%2522fid%2522%253a4006%252c%2522ms%2522%253a0%252c%2522md%2522%253a18179%252c%2522sc%2522%253a%257b%25221%2522%253a1%252c%25222%2522%253a0%252c%25223%2522%253a0%252c%25224%2522%253a0%257d%252c%2522es%2522%253a0%252c%2522at%2522%253a1%252c%2522he%2522%253a0%252c%2522da%2522%253a1%252c%2522ba%2522%253a0%252c%2522bu%2522%253a0%252c%2522job%2522%253a%257b%25220%2522%253a1%252c%25221%2522%253a1%252c%25222%2522%253a0%252c%25223%2522%253a0%252c%25224%2522%253a0%257d%252c%2522weapon%2522%253a%257b%25220%2522%253a1%252c%25221%2522%253a0%252c%25222%2522%253a0%252c%25223%2522%253a1%252c%25224%2522%253a0%252c%25225%2522%253a0%252c%25228%2522%253a0%252c%25229%2522%253a0%252c%252210%2522%253a0%257d%252c%2522box%2522%253a1%252c%2522um%2522%253a%257b%25221%2522%253a0%252c%25222%2522%253a0%252c%25223%2522%253a2%257d%252c%2522fj%2522%253a0%252c%2522fw%2522%253a0%252c%2522fo%2522%253a0%252c%2522cc%2522%253a1%252c%2522bf_atk%2522%253a0%252c%2522bf_hp%2522%253a0%252c%2522bf_spd%2522%253a0%257d%26res%3d1%26s%3d1%26time%3d1.68%26wc%3d{5}".format(hexNow, now, jid, bid, pt_cid[idx], wave_list[idx])
                 r = requests.post(post_url, data=payload, headers=self.headers, cookies=cookies).json()
-                self.logger.debug("End entry = {0}".format(r))
+                # self.logger.debug("End entry = {0}".format(r))
                 self.logger.debug('討伐關卡: {0} 完成'.format(bid))
 
 
