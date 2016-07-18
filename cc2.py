cf03e8f2f2d99dffa8ab2d7c7d99d54885c663ad
Neil_Wei <neil_wei@trend.com.tw>
2016-07-18 11:18:46 +0800

    Update

--- cc2.py
+++ cc2.py
@@ -103,18 +103,6 @@ class ChainChronicleAutomation():
             except:
                 self.config['Explorer']['area'] = 0
 
-            # try:
-            #     self.config['Explorer']['card_idx'] = config.get('Explorer', 'card_idx')
-            # except:
-            #     self.config['Explorer']['card_idx'] = 0
-
-            # try:
-            #    self.config['Explorer']['interval'] = config.get('Explorer', 'interval')
-            # except:
-            #    self.config['Explorer']['interval'] = 0
-
-            # for key in self.config:
-            #    self.logger.debug('Key = {0}, Value = {1}'.format(key, self.config[key]))
         except Exception as e:
             raise
             sys.exit(0)
@@ -150,11 +138,6 @@ class ChainChronicleAutomation():
         # self.logger.debug(r)
         try:
             self.sid = r.json()['login']['sid']
-            #all_data = self.CC_GetAllData()
-            #rings = all_data['body'][8]['data'][9]
-            #self.logger.debug("ring: {0}".format(rings))
-            #self.logger.debug("ChainChronicle Login result = [{0}]".format(r.json()['res']))
-            #self.logger.debug("session ID = {0}".format(self.sid))
         except Exception as e:
             self.logger.error("無法登入, Message = {0}".format(r.json()['msg']))
             sys.exit(0)
@@ -253,6 +236,7 @@ class ChainChronicleAutomation():
             #魔神戰
             self.__PlayRaid(bRaid)
 
+            # 避免資料不正常，需要sleep一下
             time.sleep(0.7)
 
     def CC_SetPassword(self, password):
@@ -342,7 +326,7 @@ class ChainChronicleAutomation():
                                     sys.exit(0)
 
                 except KeyError as e:
-                    self.logger.error("Key Error:{0}, 找不到卡片idx, 可能是包包已滿".format(e))
+                    self.logger.error("Key Error:{0}, 找不到卡片idx, 可能是包包已滿，或是卡片是新的".format(e))
                     sys.exit(0)
                 except Exception as e:
                     raise
@@ -597,31 +581,39 @@ class ChainChronicleAutomation():
 
 
     def __sellItem(self, idx):
-        now = int(time.time()*1000)
-        hexNow = format(now + 5000, 'x')
+        url = 'http://prod4.cc.mobimon.com.tw/card/sell'
         cookies = {'sid': self.sid}
-        self.headers = {
-                    'Cookie': 'sid={0}'.format(self.sid),
-                    'nat': "c=1&cnt=14e5c7f82a4&nature=c%3d1%26cnt%3d{0}%26t%3d6&t=6&timestamp={1}".format(hexNow, now)
-                    }
-        post_url = "http://prod4.cc.mobimon.com.tw/card/sell?c={0}&cnt={1}&timestamp={2}".format(idx, hexNow, now)
-        payload = "nature=c%3d{0}%26cnt%3d{1}".format(idx, hexNow)
-        r = requests.post(post_url, data=payload, headers=self.headers, cookies=cookies).json()
+        headers = {'Cookie': 'sid={0}'.format(self.sid)}
+        data = {
+            'c': idx
+        }
+        r = self.poster.post_data(url, headers, cookies, **data)
         return r
 
 
     def __getQuest(self, qtype, qid):
         # Get Quest
-        now = int(time.time())
-        hexNow = format(now+5000, 'x')
+        url = 'http://prod4.cc.mobimon.com.tw/quest/entry'
         cookies = {'sid': self.sid}
-        self.headers = {
-                'Cookie': 'sid={0}'.format(self.sid),
-                'nat': "cnt={0}&nature=cnt%3d{0}%26param%3d%257b%2522APP%2522%253a%257b%2522Version%2522%253a%25222.22%2522%252c%2522time%2522%253a%2522143198601943%2522%252c%2522Lang%2522%253a%2522Chinese%2522%257d%252c%2522DEV%2522%253a%257b%2522Model%2522%253a%2522Xiaomi%2bMI%2b3W%2522%252c%2522CPU%2522%253a%2522ARMv7%2bVFPv3%2bNEON%2522%252c%2522GPU%2522%253a%2522Adreno%2b(TM)%2b330%2522%252c%2522OSVersion%2522%253a%2522Android%2bOS%2b4.4.4%2b%252f%2bAPI-19%2b(KTU84P%252fV6.5.3.0.KXDMICD)%2522%252c%2522UserUniqueID%2522%253a%2522{2}%2522%252c%2522SysRAM%2522%253a1850%252c%2522VideoRAM%2522%253a198%252c%2522OS%2522%253a%25222%2522%252c%2522Token%2522%253a%2522{3}%2522%257d%257d&param=%7b%22APP%22%3a%7b%22Version%22%3a%222.22%22%2c%22time%22%3a%22143198601943%22%2c%22Lang%22%3a%22Chinese%22%7d%2c%22DEV%22%3a%7b%22Model%22%3a%22Xiaomi+MI+3W%22%2c%22CPU%22%3a%22ARMv7+VFPv3+NEON%22%2c%22GPU%22%3a%22Adreno+(TM)+330%22%2c%22OSVersion%22%3a%22Android+OS+4.4.4+%2f+API-19+(KTU84P%2fV6.5.3.0.KXDMICD)%22%2c%22UserUniqueID%22%3a%22{2}%22%2c%22SysRAM%22%3a1850%2c%22VideoRAM%22%3a198%2c%22OS%22%3a%222%22%2c%22Token%22%3a%22{3}%22%7d%7d&timestamp={1}".format(hexNow, now, self.config['General']['uid'], self.config['General']['token'])
-                }
-        post_url = "http://prod4.cc.mobimon.com.tw/quest/entry?oc=1&type={3}&qid={0}&fid=1965350&pt=0&cnt={1}&timestamp={2}".format(qid, hexNow, now, qtype)
-        payload = "nature=cnt%3d{0}%26fid%3d1965350%26pt%3d0%26qid%3d{2}%26type%3d{1}".format(hexNow, qtype, qid)
-        r = requests.post(post_url, data=payload, headers=self.headers, cookies=cookies)
+        headers = {'Cookie': 'sid={0}'.format(self.sid)}
+        data = {
+            'oc': 1,
+            'type': qtype,
+            'qid': qid,
+            'fid': 1965350,
+            'pt': 0
+        }
+        r = self.poster.post_data(url, headers, cookies, **data)
+        # now = int(time.time())
+        # hexNow = format(now+5000, 'x')
+        # cookies = {'sid': self.sid}
+        # self.headers = {
+        #         'Cookie': 'sid={0}'.format(self.sid),
+        #         'nat': "cnt={0}&nature=cnt%3d{0}%26param%3d%257b%2522APP%2522%253a%257b%2522Version%2522%253a%25222.22%2522%252c%2522time%2522%253a%2522143198601943%2522%252c%2522Lang%2522%253a%2522Chinese%2522%257d%252c%2522DEV%2522%253a%257b%2522Model%2522%253a%2522Xiaomi%2bMI%2b3W%2522%252c%2522CPU%2522%253a%2522ARMv7%2bVFPv3%2bNEON%2522%252c%2522GPU%2522%253a%2522Adreno%2b(TM)%2b330%2522%252c%2522OSVersion%2522%253a%2522Android%2bOS%2b4.4.4%2b%252f%2bAPI-19%2b(KTU84P%252fV6.5.3.0.KXDMICD)%2522%252c%2522UserUniqueID%2522%253a%2522{2}%2522%252c%2522SysRAM%2522%253a1850%252c%2522VideoRAM%2522%253a198%252c%2522OS%2522%253a%25222%2522%252c%2522Token%2522%253a%2522{3}%2522%257d%257d&param=%7b%22APP%22%3a%7b%22Version%22%3a%222.22%22%2c%22time%22%3a%22143198601943%22%2c%22Lang%22%3a%22Chinese%22%7d%2c%22DEV%22%3a%7b%22Model%22%3a%22Xiaomi+MI+3W%22%2c%22CPU%22%3a%22ARMv7+VFPv3+NEON%22%2c%22GPU%22%3a%22Adreno+(TM)+330%22%2c%22OSVersion%22%3a%22Android+OS+4.4.4+%2f+API-19+(KTU84P%2fV6.5.3.0.KXDMICD)%22%2c%22UserUniqueID%22%3a%22{2}%22%2c%22SysRAM%22%3a1850%2c%22VideoRAM%22%3a198%2c%22OS%22%3a%222%22%2c%22Token%22%3a%22{3}%22%7d%7d&timestamp={1}".format(hexNow, now, self.config['General']['uid'], self.config['General']['token'])
+        #         }
+        # post_url = "http://prod4.cc.mobimon.com.tw/quest/entry?oc=1&type={3}&qid={0}&fid=1965350&pt=0&cnt={1}&timestamp={2}".format(qid, hexNow, now, qtype)
+        # payload = "nature=cnt%3d{0}%26fid%3d1965350%26pt%3d0%26qid%3d{2}%26type%3d{1}".format(hexNow, qtype, qid)
+        # r = requests.post(post_url, data=payload, headers=self.headers, cookies=cookies)
         return r
 
     def __getBattleResult(self, qid):
