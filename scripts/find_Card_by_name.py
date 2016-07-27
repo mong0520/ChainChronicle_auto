f64b7d0b23bbddc81b1db14b8c13f4d4955650a5
Neil_Wei <neil_wei@trend.com.tw>
2016-07-27 14:39:19 +0800

    Update

--- scripts/find_Card_by_name.py
+++ scripts/find_Card_by_name.py
@@ -1,12 +1,16 @@
 from pymongo import MongoClient
 import sys
+import json
+
 client = MongoClient('127.0.0.1', 27017)
 collection = client.cc.charainfo
 
 try:
-    quest_doc = collection.find({"name": {"$regex": sys.argv[1]} })
+    #quest_doc = collection.find({"name": {"$regex": sys.argv[1]} })
+    quest_doc = collection.find({"name": {"$regex": sys.argv[1]} }, {"_id": 0})
     for doc in quest_doc:
-        #print doc
+        #print json.dumps(doc, encoding="UTF-8", ensure_ascii=False)
+        # print doc
         print doc['cid']
         print "%s %s" % (doc['title'], doc['name'])
 except KeyError as e:
