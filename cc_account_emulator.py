f4b37e48e8af02c093705f477d9189d7ae48a20d
Neil_Wei <neil_wei@trend.com.tw>
2016-10-21 12:10:19 +0800

    Update emulator

--- cc_account_emulator.py
+++ cc_account_emulator.py
@@ -8,15 +8,23 @@ import argparse
 
 
 class CCAccountEmulator(threading.Thread):
-    def __init__(self, config, uuid_list):
+    def __init__(self, thread_id, config, uuid_list, target_ac_point):
         threading.Thread.__init__(self)
+        self.thread_id = thread_id
         self.cc = ChainChronicle(config, console_log_level=logging.CRITICAL)
         self.cc.load_config()
         self.uuid_list = uuid_list
         self.counter = 0
-
+        self.target_ac_point = target_ac_point
 
     def run(self):
+        earned_ac_point = 0
+
+        # not garuanteed the accuracy
+        quest_count = self.cc.config.getint('QUEST', 'Count')
+        ac_earn_in_batch = int(10 * quest_count)
+        print 'Earned AC for one UUID in {0} counts: {1}'.format(quest_count, ac_earn_in_batch)
+
         while True:
             try:
                 uuid = self.uuid_list[self.counter].strip()
@@ -25,12 +33,21 @@ class CCAccountEmulator(threading.Thread):
                 self.cc.account_info['uid'] = uuid
                 print "Use uuid {0}".format(uuid)
                 self.cc.start()
+                earned_ac_point += ac_earn_in_batch
+                if self.target_ac_point is not None:
+                    if earned_ac_point >= self.target_ac_point:
+                        print 'Thread:{0} earned AC point: {1}, Reach target AC point: {2}, mission is completed'.format(
+                            self.thread_id, earned_ac_point, self.target_ac_point)
+                        break
+                    else:
+                        print 'Thread:{0} earned AC point: {1}, target AC point: {2}'.format(
+                            self.thread_id, earned_ac_point, self.target_ac_point)
             except Exception as e:
                 if self.counter >= len(self.uuid_list):
                     print 'reset uuid'
                     self.counter = 0
                 else:
-                    traceback.print_exc()  
+                    # traceback.print_exc()
                     print 'select next uuid'
                     self.counter += 1 
 
@@ -46,8 +63,11 @@ def main():
     parser.add_argument('-c', '--config', help='fake account template', required=True, action='store')
     parser.add_argument('-f', '--uuid_file', help='predefined uuid list', required=True, action='store')
     parser.add_argument('-t', '--thread', help='thread count', required=True, action='store', type=int)
+    parser.add_argument('-p', '--ac_point', help='target AC point to earn', required=True, action='store', type=int,
+                        default=None)
     args = parser.parse_args()
-  
+
+    target_ac = args.ac_point
     thread_count = args.thread
     threads = list()
     with open(args.uuid_file, 'r') as uuid:
@@ -59,16 +79,25 @@ def main():
     # print len(uuid_chunk_list)
     # print len(uuid_chunk_list[0])
     # sys.exit(0)
+    if target_ac and target_ac >= 0:
+        target_ac_per_thread = args.ac_point / thread_count
+    else:
+        target_ac_per_thread = None
+
+    print 'Target AC point = {0}, thread count = {1}, so each thread should earn at least {2} AC point'.format(
+        args.ac_point, thread_count, target_ac_per_thread
+    )
+
     for i in xrange(0, thread_count):
         # threads.append(CCAccountEmulator('config/fake_account.conf', uuid_list))
-        threads.append(CCAccountEmulator(args.config, uuid_chunk_list[i]))
+        threads.append(CCAccountEmulator(i, args.config, uuid_chunk_list[i], target_ac_per_thread))
 
     for t in threads:
-        print 'Thread {0} starts'.format(t)
+        print 'Thread {0} starts'.format(t.thread_id)
         t.start()
 
     for t in threads:
-        print 'Main thread starts to waiting Thread: {0}'.format(t)
+        print 'Main thread starts to waiting Thread: {0}'.format(t.thread_id)
         t.join()
 
 if __name__ == '__main__':
