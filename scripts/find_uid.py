fe91338ca335917955b2e7937b4a0c3d4d28b75b
Neil_Wei <neil_wei@trend.com.tw>
2016-10-21 16:46:20 +0800

    update

--- scripts/find_uid.py
+++ scripts/find_uid.py
@@ -10,7 +10,7 @@ import argparse
 import find_cards
 import utils
 
-VALID_KEYS = ['uid', 'lv', 'sup_lv', 'name', 'card']
+VALID_KEYS = ['uid', 'lv', 'name']
 
 
 def get_sid():
@@ -43,17 +43,21 @@ def query_friend(sid, oid):
 def main():
     parser = argparse.ArgumentParser(description="Chain Chronicle Query tool")
     parser.add_argument('-i', '--oid', help='Open ID', required=True, action='store')
+    parser.add_argument('-v', '--verbose', help='dump leader or not', required=False, action='store_true')
     args = parser.parse_args()
 
     sid = get_sid()
     result = query_friend(sid, args.oid)
     if 'friend' in result:
         for k, v in result['friend'].iteritems():
-            if k in VALID_KEYS:
+            if args.verbose:
                 if k == 'card':
-                    r = utils.db_operator.DBOperator.dump_cards('cid', v['id'])
-                    print r
+                        r = utils.db_operator.DBOperator.get_cards('cid', v['id'])
+                        print u'Leader = {0}'.format(r[0]['title'] + r[0]['name'])
                 else:
+                    print u"{0} = {1}".format(k, v)
+            else:
+                if k in VALID_KEYS:
                     print u"{0} = {1}".format(k, v)
     else:
         print 'Query failed'
