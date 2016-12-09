36aa08d455f3e7eae8ef0162be55d2ff0e0bdd43
Neil_Wei <mongcheng@gmail.com>
2016-12-09 18:25:37 +0800

    fix

--- lib/gacha_client.py
+++ lib/gacha_client.py
@@ -18,9 +18,9 @@ def gacha(parammeter, sid):
         't': parammeter['type'],
         'c': parammeter['batch_count']
     }
-    if parammeter['area']:
+    if 'area' in parammeter and parammeter['area']:
         data['area'] = parammeter['area']
-    if parammeter['place']:
+    if 'place' in parammeter and parammeter['place']:
         data['place'] = parammeter['place']
     r = poster.post_data(url, headers, cookies, payload=None, **data)
     time.sleep(sleep_time)
