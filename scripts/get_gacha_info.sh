92b294d3dcd3f07c205bafa6842db774f0dad92e
MongCheng <mongcheng@gmail.com>
2017-03-25 13:41:35 +0800

    update

--- scripts/get_gacha_info.sh
+++ scripts/get_gacha_info.sh
@@ -3,6 +3,6 @@
 rm -f result.txt
 rm -f temp.html
 python gacha_info.py > temp.html
-cat temp.html | grep 'event_id' > result.txt
+cat temp.html | grep 'event_id' | cut -d '&' -f 2-> result.txt
 cat temp.html | grep -A 5 'var map_event_gacha_type' >> result.txt
 cat result.txt
