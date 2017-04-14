dcaf6723a25c1b8392a54efc213574ca1ed4ea95
MongCheng <mongcheng@gmail.com>
2017-04-14 18:33:42 +0800

    Update

--- scripts/PostActionForJpg.sh
+++ scripts/PostActionForJpg.sh
@@ -2,6 +2,6 @@
 # This script should be run after decode scr files in Windows
 rm -f resource/*.scr
 mv resource/*.jpg ../jpg/
-python resize_image ../jpg/
+python resize_image.py ../jpg/
 cd ../
 zip -9 cc_image.zip img/*
