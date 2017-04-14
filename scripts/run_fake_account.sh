48b502e81dbed92a784fa5b7a01e3aef7b592b29
MongCheng <mongcheng@gmail.com>
2017-04-14 18:26:40 +0800

    Update

--- scripts/run_fake_account.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ../
-python cc_account_emulator_simple2.py -f uuid-1.txt -t 200 > /tmp/uuid-1.log 2>&1 &
-python cc_account_emulator_simple2.py -f uuid-2.txt -t 200 > /tmp/uuid-2.log 2>&1 &
-python cc_account_emulator_simple2.py -f uuid-3.txt -t 200 > /tmp/uuid-3.log 2>&1 &
-python cc_account_emulator_simple2.py -f uuid-4.txt -t 200 > /tmp/uuid-4.log 2>&1 &
